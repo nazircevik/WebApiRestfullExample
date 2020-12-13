ee04d714380d896eabf67d04f461acd5178462bf
Nazir Çevik <nzr.cevik@hotmail.com>
2020-12-13 19:55:29 +0300

    First Commit

---
+++ NazirCevik.WebApiDemo/NazirCevik.WebApiDemo/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NazirCevik.WebApiDemo.Controllers
+{
+    [Route("api/products")]
+    public class ProductsController:Controller
+    {
+        [HttpGet("")]
+        public string Get()
+        {
+            return "product1sadasdasdsa asdas d asd as das dasdasd";
+        }
+    }
+}
